f39b17829d3226363a9935dc28b7ce7078f74509
Nicat <bayramov.nicat@hotmail.com>
2021-10-07 12:11:47 +0400

    furnishing

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import data from 'data/data.json';
 import jewellery from 'data/jewellery.json';
+import furnishing from 'data/furnishing.json';
 import refining from 'data/refining.json';
 
 @Component({
@@ -11,7 +12,7 @@ import refining from 'data/refining.json';
 export class ItemsComponent implements OnInit {
   items: any[];
   constructor() {
-    this.items = data.concat(refining).concat(jewellery);
+    this.items = data.concat(refining).concat(jewellery).concat(furnishing);
   }
 
   // Filter
@@ -55,12 +56,12 @@ export class ItemsComponent implements OnInit {
   }
 
   // Categories
-  categories: string[] = [];
+  categories: string[] = ['Trinkets', 'Furniture'];
   ngOnInit() {
-    this.items.forEach(item => {
-      if (this.categories.indexOf(item.category) == -1)
-        this.categories.push(item.category);
-    });
+    // this.items.forEach(item => {
+    //   if (this.categories.indexOf(item.category) == -1)
+    //     this.categories.push(item.category);
+    // });
     // this.choosedItem = <any>{ "id": "TimberT5", "name": "Ironwood Planks", "rarity": 0, "icon": "timbert5", "itemType": "Resource", "ingredients": [{ "type": "item", "itemType": "Resource", "id": "WoodT5", "name": "Ironwood", "rarity": 0, "icon": "WoodT5", "quantity": 8 }, { "type": "item", "itemType": "Resource", "id": "TimberT4", "name": "Wyrdwood Planks", "rarity": 0, "icon": "TimberT4", "quantity": 2 }, { "type": "category", "name": "Sandpaper", "quantity": 1, "subIngredients": [{ "type": "item", "itemType": "Resource", "id": "SandpaperT3", "name": "Coarse Sandpaper", "rarity": 0, "icon": "SandpaperT3", "quantity": 1, "gsMaxBonus": 0, "gsMinBonus": 0 }, { "type": "item", "itemType": "Resource", "id": "SandpaperT4", "name": "Fine Sandpaper", "rarity": 1, "icon": "SandpaperT4", "quantity": 1, "gsMaxBonus": 0, "gsMinBonus": 0 }, { "type": "item", "itemType": "Resource", "id": "SandpaperT5", "name": "Obsidian Sandpaper", "rarity": 2, "icon": "SandpaperT5", "quantity": 1, "gsMaxBonus": 0, "gsMinBonus": 0 }] }], "tradeskill": "Woodworking", "recipeLevel": 150, "category": "Refined Resources", "stations": ["Woodshop Tier 5"], "event": { "CategoricalProgressionId": "Woodworking", "CategoricalProgressionReward": 770, "TerritoryStanding": 0 } };
   }
 }
